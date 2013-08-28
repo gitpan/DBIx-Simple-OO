aaeef25426348710fa3220c20e5a47da61b4c802
Chris Williams <chris@bingosnet.co.uk>
2013-08-28 14:56:12 +0000

    Import of BINGOS/DBIx-Simple-OO-0.02 from CPAN.
    gitpan-cpan-distribution: DBIx-Simple-OO
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BINGOS/DBIx-Simple-OO-0.02.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/DBIx/Simple/OO.pm
+++ lib/DBIx/Simple/OO.pm
@@ -5,16 +5,6 @@ use vars qw[$VERSION];
 
 $VERSION = 0.02;
 
-use DBIx::Simple;
-
-if ($DBIx::Simple::VERSION >= 1.33) {
-    # Monkey patch an @ISA instead of ::Result for recent versions of
-    # DBIx::Simple, because they support ->object and ->objects out of the
-    # box -- but with a different class name.
-
-    push @DBIx::Simple::Result::RowObject::ISA, 'DBIx::Simple::OO::Item';
-} else {
-
 =head1 NAME
 
 DBIx::Simple::OO
@@ -37,9 +27,9 @@ DBIx::Simple::OO
     $age    = $obj->age;            # get the value for field 'age'
 
     @acc    = $obj->ls_accessors;   # get a list of all fields
-    $sub    = $obj->can('name');    # check if this object has a 
+    $sub    = $obj->can('name');    # check if this object has a
                                     # 'name' method
-                                    
+
     ### add a method to every object that will be returned
     ### by DBIx::Simple::OO
     {   package DBIx::Simple::OO::Item;
@@ -49,14 +39,7 @@ DBIx::Simple::OO
 
 =head1 DESCRIPTION
 
-B<DBIx::Simple now has support for C<object> and C<objects> built into
-it.> I<Using DBIx::Simple::OO is no longer necessary if you change
-C<DBIx::Simple::OO::Item> to C<DBIx::Simple::Result::RowObject> and
-use 1.33 or newer. DBIx::Simple::OO 0.02 implements a work-around to
-avoid name clashes, but using 0.01 with DBIx::Simple 1.33+ could go
-wrong.>
-
-This module provides a possibility to retrieve rows from a 
+This module provides a possibility to retrieve rows from a
 database as objects, rather than the traditional C<array ref>
 or C<hash ref>. This provides all the usual benefits of using
 objects over plain references for accessing data, as well as
@@ -65,7 +48,7 @@ retrieval.
 
 =head1 HOW IT WORKS
 
-C<DBIx::Simple::OO> declares its 2 methods in the C<DBIx::Simple::Result>
+C<DBIx::Simple::OO> declares it's 2 methods in the C<DBIx::Simple::Result>
 namespace, transforming the rows retrieved from the database to
 full fledged objects.
 
@@ -73,23 +56,25 @@ full fledged objects.
 
 ### the retrieval methods are in the DBIx::Simple::Result package
 
+package DBIx::Simple::Result;
+
 =head1 METHODS
 
 This module subclasses C<DBIx::Simple> and only adds the following
 methods. Any other method, like the C<new> call should be looked up
 in the C<DBIx::Simple> manpage instead.
 
-=head2 $obj = $db->query(....)->object( );
+=head2 $obj = = $db->query(....)->object( );
 
 Returns the first result from your query as an object.
 
 =cut
 
-    *DBIx::Simple::Result::object = sub {
-        my $self = shift or return;
+sub object {
+    my $self = shift or return;
 
-        return $self->_href_to_obj( $self->hash );
-    };
+    return $self->_href_to_obj( $self->hash );
+}
 
 =head2 @objs = $db->query(....)->objects( );
 
@@ -97,36 +82,34 @@ Returns the results from your query as a list of objects.
 
 =cut
 
-    *DBIx::Simple::Result::objects = sub {
-        my $self = shift or return;
-     
-        return map { $self->_href_to_obj( $_ ) } $self->hashes;
-    };
+sub objects {
+    my $self = shift or return;
 
-    ### convert the hashref to a nice O::A object
+    return map { $self->_href_to_obj( $_ ) } $self->hashes;
 }
 
-*DBIx::Simple::Result::_href_to_obj = sub {
+### convert the hashref to a nice O::A object
+sub _href_to_obj {
     my $self = shift or return;
     my $href = shift or return;
-    
+
     my $obj  = DBIx::Simple::OO::Item->new;
-    
+
     ### create accessors for every hash key
     $obj->mk_accessors( keys %$href );
-    
+
     ### and set the value
     for my $acc ( $obj->ls_accessors ) {
         $obj->$acc( $href->{$acc} );
     }
 
     return $obj;
-};
+}
 
 =head1 ACCESSORS
 
-All objects returned by the above methods are from the 
-C<DBIx::Simple::OO::Item> class, which subclasses 
+All objects returned by the above methods are from the
+C<DBIx::Simple::OO::Item> class, which subclasses
 C<Object::Accessor>.
 
 The most important methods are described in the synopsis, but
@@ -134,8 +117,8 @@ you should refer to the C<Object::Accessor> manpage for more
 extensive documentation.
 
 Note that it is possible to declare methods into the
-C<DBIx::Simple::OO::Item> class to extend the functionality 
-of the objects returned by C<DBIx::Simple::OO>, as also 
+C<DBIx::Simple::OO::Item> class to extend the functionality
+of the objects returned by C<DBIx::Simple::OO>, as also
 described in the C<SYNOPSIS>
 
 =cut
@@ -147,19 +130,16 @@ use base 'Object::Accessor';
 
 1;
 
+=head1 BUG REPORTS
+
+Please report bugs or other issues to E<lt>bug-dbix-simple-oo@rt.cpan.org<gt>.
+
 =head1 AUTHOR
 
-This module by
-Jos Boumans E<lt>kane@cpan.orgE<gt>.
+This module by Jos Boumans E<lt>kane@cpan.orgE<gt>.
 
 =head1 COPYRIGHT
 
-This module is
-copyright (c) 2005 Jos Boumans E<lt>kane@cpan.orgE<gt>.
-All rights reserved.
-
-This library is free software;
-you may redistribute and/or modify it under the same
-terms as Perl itself.
-
+This library is free software; you may redistribute and/or modify it
+under the same terms as Perl itself.
 =cut
--- t/02_select.t
+++ t/02_select.t
@@ -1,62 +1,55 @@
 use Test::More 'no_plan';
 use strict;
 
-BEGIN { 
-    chdir 't' if -d 't'; 
-    
+BEGIN {
+    chdir 't' if -d 't';
+
     use File::Spec;
     use lib File::Spec->catdir(qw[.. lib]), 'inc';
 
     require 'conf.pl';
 }
 
-### dummy package declaration to return some 
-{   package DBIx::Simple::Result::Mock;
-    push @DBIx::Simple::Result::Mock::ISA, 'DBIx::Simple::Result';
-   
-    my $counter = 0; 
+### dummy package declaration to return some
+{   package DBIx::Simple::OO::Mock;
+
+    ### make it use these methods instead, for testing purposes
+    unshift @DBIx::Simple::Result::ISA, __PACKAGE__;
+
 
     ### declare the DBIx::Simple::Result methods
-    sub _die { }
-
-    # DBIx::Simple >= 1.33
-    sub array   { return [ 1, 2 ] if ++$counter < 4; return; }
-    sub columns { return 'a', 'b' }
-
-    # DBIx::Simple <= 1.32
     sub hash    { return { a => 1, b => 2 } }
     sub hashes  { return { a => 1 }, { b => 2 } }
-}    
+}
 
 
 my $Class   = 'DBIx::Simple::OO';
 my $OClass  = $Class . '::Item';
-
-my $RObj = bless {}, 'DBIx::Simple::Result::Mock';
+my $RClass  = 'DBIx::Simple::Result';
 
 use_ok( $Class );
 
 {   my %meth = (            # amount of objects returned
-        object      => 1,   
+        object      => 1,
         objects     => 2,
-    );            
+    );
 
     while (my ($meth,$cnt) = each %meth) {
 
-        can_ok( $RObj,         $meth );
+        can_ok( $RClass,         $meth );
 
-        my @res = $RObj->$meth( );
+        my @res = $RClass->$meth( );
         is( scalar(@res), $cnt, "   Got $cnt results for '$meth'" );
-        
+
         for my $obj (@res) {
             ok( $obj,           "   Retrieved object" );
 
             ### ... isa foo will be added by Test::More
             isa_ok( $obj, $OClass,
                                 "       Object" );
-        
+
             my @acc = $obj->ls_accessors;
             ok( scalar(@acc),   "       Accessors retrieved" );
         }
     }
-}    
+}
